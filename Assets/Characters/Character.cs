71feef109bcb626c5c60cd404404c7d483a23b4a
JustinDC <JustinConcepcion2015@u.northwestern.edu>
2015-05-26 03:27:26 -0500

    Added animating sprites to character prefab

--- Assets/Characters/Character.cs
+++ Assets/Characters/Character.cs
@@ -17,8 +17,16 @@ public class Character : MonoBehaviour {
 	public KeyCode interact;
 	public KeyCode attack;
 
+	// sprites
+	public GameObject head;
+	public GameObject body;
+	public GameObject legs;
+	private Animator headAnimator;
+	private Animator bodyAnimator;
+	private Animator legsAnimator;
+
 	// movement variables
-	const float speed = 2.5f;
+	const float speed = 1.0f;
 	private List<Direction> movement; // 0: Left, 1: Up, 2: Right, 3: Down
 	private Direction currentMove;
 
@@ -37,6 +45,13 @@ public class Character : MonoBehaviour {
 		currentAction = Action.Wait;
 		moveTo = transform.localPosition;
 		actionTimer = 0;
+
+		headAnimator = head.GetComponent<Animator>();
+		bodyAnimator = body.GetComponent<Animator>();
+		legsAnimator = legs.GetComponent<Animator>();
+		headAnimator.Play("idle", -1, float.NegativeInfinity);
+		bodyAnimator.Play("idle", -1, float.NegativeInfinity);
+		legsAnimator.Play ("idle", -1, float.NegativeInfinity);
 	}
 
 	void PlayerMovement() {
@@ -174,14 +189,22 @@ public class Character : MonoBehaviour {
 		}
 
 		Rigidbody2D body = GetComponent<Rigidbody2D>();
+
+		if (currentMove != Direction.None) {
+			headAnimator.Play ("walk", -1, float.NegativeInfinity);
+			bodyAnimator.Play ("walk", -1, float.NegativeInfinity);
+			legsAnimator.Play ("walk", -1, float.NegativeInfinity);
+		}
 		switch (currentMove) {
 		case Direction.Left:
+			transform.localScale = new Vector3 (-1, 1, 1);
 			body.velocity = new Vector2 (-speed, 0.0f);
 			break;
 		case Direction.Up:
 			body.velocity = new Vector2 (0.0f, speed);
 			break;
 		case Direction.Right:
+			transform.localScale = new Vector3 (1, 1, 1);
 			body.velocity = new Vector2 (speed, 0.0f);
 			break;
 		case Direction.Down:
@@ -189,6 +212,9 @@ public class Character : MonoBehaviour {
 			break;
 		default:
 			body.velocity = new Vector2 (0.0f, 0.0f);
+			headAnimator.Play ("head", -1, float.NegativeInfinity);
+			bodyAnimator.Play ("idle", -1, float.NegativeInfinity);
+			legsAnimator.Play ("idle", -1, float.NegativeInfinity);
 			break;
 		}
 	}
