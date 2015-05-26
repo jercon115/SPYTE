4dbcd341e2dce1b812c791de454d5293b3519ca4
JustinDC <JustinConcepcion2015@u.northwestern.edu>
2015-05-26 05:31:24 -0500

    Added scenes for winning

---
+++ Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetSceneGame () {
+		Application.LoadLevel (0);
+		Debug.Log("Button was pressed");
+	}
+
+	public void SetScenePlayer1Wins () {
+		Application.LoadLevel ("Player1Wins");
+	}
+
+	public void SetScenePlayer2Wins () {
+		Application.LoadLevel ("Player2Wins");
+	}
+}
