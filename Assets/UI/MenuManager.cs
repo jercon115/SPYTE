176dbf4f743bb9f62ba847213f214cdc794cbe4a
JustinDC <JustinConcepcion2015@u.northwestern.edu>
2015-06-05 01:23:19 -0500

    Added menu and speech bubble art

---
+++ Assets/UI/MenuManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void loadScene(string scene) {
+		Application.LoadLevel (scene);
+	}
+}
