067f28a8fa0d1b8e8641a4910066289546652c2d
jercon115 <jeremi.concepcion@gmail.com>
2015-06-05 15:20:08 -0500

    Made speech bubble's timers work based on Time.deltaTime

--- Assets/UI/SpeechBubble.cs
+++ Assets/UI/SpeechBubble.cs
@@ -7,21 +7,21 @@ public class SpeechBubble : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		timer = 90;
+		timer = 2.5f;
 		transform.localScale = new Vector3(0.0f,0.0f,0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (new Vector3 (0.0f, 0.005f));
+		transform.Translate (new Vector3 (0.0f, 0.2f*Time.deltaTime));
 
 		if (timer > 0) {
-			if (timer > 30) {
+			if (timer > 1.25f) {
 				transform.localScale += (new Vector3(1.0f,1.0f,1.0f) - transform.localScale)*0.1f;
 			} else
 				transform.localScale += (new Vector3(0.0f,0.0f,0.0f) - transform.localScale)*0.1f;
 
-			timer--;
+			timer -= Time.deltaTime;
 		} else
 			Destroy (gameObject);
 	}
